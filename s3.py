56ab3667e4ade7caf37d5cee872155a015c761a4
KEVIN32-23 <105031169+KEVIN32-23@users.noreply.github.com>
2022-05-06 08:36:43 -0500

    Add files via upload
    s3

---
+++ s3.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  6 08:02:55 2022
+
+@author: Kevin Pilco 
+"""
+
+lista1=[6,589.8,True,False,
+        "Cisco",5,8,"Networking",85.9]
+print(type(lista1))
+print(lista1)
+print(lista1[0])
+print(lista1[8])
+print(lista1[-9])
+print(lista1[-1])
+lista1[0]="pythom"
+del lista1[8]
+tupla1=(6,589.8,True,False
+        "Cisco",5,8,"Networking")
+print(type(tupla1))
+print(tupla(1))
+print(tupla[0])
