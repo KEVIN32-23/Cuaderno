aef51567defebe48891d753a82df30cc66d2fddf
KEVIN32-23 <105031169+KEVIN32-23@users.noreply.github.com>
2022-05-06 08:28:52 -0500

    Add files via upload
    Trabajo en clase 6/5/2022

---
+++ c1gh.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  6 08:14:52 2022
+
+@author: Kevin Pilco
+"""
+
+dict1={"Kevin":"10.10.10.1",
+       1:"1753972619",
+       "Telefono":"2975075",
+       "Ciudad":"Quito",
+       501:"Estudiante1",}
+
+print(dict1)
+print(dict1["Kevin"])
+print(dict1[1])
+print(dict1["Telefono"])
+dict1["CodEstudiante"]="2000"
+dict1["RT"]="10.10.10.1"
